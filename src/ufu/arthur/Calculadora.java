a5591c69ffc04083260906558efa8a55ff8a7894
Arthur <arthurrodri015@gmail.com>
2021-09-21 19:56:01 -0300

    Changes made

--- src/ufu/arthur/Calculadora.java
+++ src/ufu/arthur/Calculadora.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Calculadora {
 
     Scanner teclado = new Scanner(System.in);
-    //public float n1 = teclado.nextFloat();
-    //public float n2 = teclado.nextFloat();
     public float resultado;
 
     public void act(){
@@ -16,7 +14,8 @@ public class Calculadora {
         System.out.println("1 - Soma " +
                 "2 - Subtraçao " +
                 "3 - Multiplicaçao " +
-                "4 - Divisao");
+                "4 - Divisao" +
+                "0 - Para sair da calculadora");
         System.out.println("Digite a opçao desejada");
         n = teclado.nextInt();
 
@@ -31,15 +30,12 @@ public class Calculadora {
                 System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
                 n = teclado.nextInt();
                 if (n == 0){
-                    System.out.println("Obrigado por usar a calculadora");
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
                 } else if (n == 1){
-                    System.out.println("Ola, escolha qual operaçao voce deseja realizar: ");
-                    System.out.println("1 - Soma " +
-                            "2 - Subtraçao " +
-                            "3 - Multiplicaçao " +
-                            "4 - Divisao");
-                    System.out.println("Digite a opçao desejada");
+                    act();
                 }
+                break;
             case 2 :
                 System.out.println("Digite o primeiro numero: ");
                 n1 = teclado.nextInt();
@@ -47,6 +43,14 @@ public class Calculadora {
                 n2 = teclado.nextInt();
                 resultado = n1 - n2;
                 System.out.println("O resultado da subtraçao eh = " + resultado);
+                System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                n = teclado.nextInt();
+                if (n == 0){
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
+                } else if (n == 1){
+                    act();
+                }
                 break;
             case 3 :
                 System.out.println("Digite o primeiro numero: ");
@@ -55,6 +59,14 @@ public class Calculadora {
                 n2 = teclado.nextInt();
                 resultado = n1 * n2;
                 System.out.println("O resultado da multiplicacao eh = " + resultado);
+                System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                n = teclado.nextInt();
+                if (n == 0){
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
+                } else if (n == 1){
+                    act();
+                }
                 break;
             case 4 :
                 System.out.println("Digite o primeiro numero: ");
@@ -63,9 +75,18 @@ public class Calculadora {
                 n2 = teclado.nextInt();
                 resultado = n1 / n2;
                 System.out.println("O resultado da divisao eh = " + resultado);
+                System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                n = teclado.nextInt();
+                if (n == 0){
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
+                } else if (n == 1){
+                    act();
+                }
                 break;
             default :
-                System.out.println("Opçao nao disponivel");
+                System.out.println("Opçao nao disponivel!!");
+                break;
         }
     }
 
