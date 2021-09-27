8428cadc4daddd7b20f7cd510a09085fb69c6af5
Arthur <arthurrodri015@gmail.com>
2021-09-27 19:02:18 -0300

    Introducing trigonometry in the project

--- src/ufu/arthur/Calculadora.java
+++ src/ufu/arthur/Calculadora.java
@@ -2,134 +2,212 @@ package ufu.arthur;
 
 import java.util.Scanner;
 
-public class Calculadora {
+public class Calculadora  {
 
     Scanner teclado = new Scanner(System.in);
-<<<<<<< HEAD
-=======
-    //public float n1 = teclado.nextFloat();
-    //public float n2 = teclado.nextFloat();
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
-    public float resultado;
+    public double resultado;
 
-    public void act(){
+    public void act() {
         int n;
         float n1, n2;
+        double d1, d2;
         System.out.println("Ola, escolha qual operaçao voce deseja realizar: ");
-        System.out.println("1 - Soma " +
-                "2 - Subtraçao " +
-                "3 - Multiplicaçao " +
-<<<<<<< HEAD
-                "4 - Divisao" +
-                "5 - Potencia" +
-                "0 - Para sair da calculadora");
-=======
-                "4 - Divisao");
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
+        System.out.println("1 - Soma\n" +
+                "2 - Subtraçao\n" +
+                "3 - Multiplicaçao\n" +
+                "4 - Divisao\n" +
+                "5 - Potencia\n" +
+                "6 - Raiz quadrada\n" +
+                "7 - Fatorial\n" +
+                "8 - Trigonometria\n" +
+                "0 - Para sair da calculadora\n");
         System.out.println("Digite a opçao desejada");
         n = teclado.nextInt();
 
-        switch (n){
-            case 1 :
+        switch(n){
+            case 1:
                 System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextInt();
+                n1 = teclado.nextFloat();
                 System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextInt();
+                n2 = teclado.nextFloat();
                 resultado = n1 + n2;
+                resultado = 6;
                 System.out.println("O resultado da soma eh = " + resultado);
                 System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
                 n = teclado.nextInt();
-                if (n == 0){
-<<<<<<< HEAD
+                if (n == 0) {
                     System.out.println("Obrigado por usar a calculadora, volte sempre!");
                     break;
-                } else if (n == 1){
+                } else if (n == 1) {
                     act();
                 }
-                break;
-=======
-                    System.out.println("Obrigado por usar a calculadora");
-                } else if (n == 1){
-                    System.out.println("Ola, escolha qual operaçao voce deseja realizar: ");
-                    System.out.println("1 - Soma " +
-                            "2 - Subtraçao " +
-                            "3 - Multiplicaçao " +
-                            "4 - Divisao");
-                    System.out.println("Digite a opçao desejada");
-                }
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
-            case 2 :
+            break;
+
+            case 2:
                 System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextInt();
+                n1 = teclado.nextFloat();
                 System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextInt();
+                n2 = teclado.nextFloat();
                 resultado = n1 - n2;
                 System.out.println("O resultado da subtraçao eh = " + resultado);
-<<<<<<< HEAD
                 System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
                 n = teclado.nextInt();
-                if (n == 0){
+                if (n == 0) {
                     System.out.println("Obrigado por usar a calculadora, volte sempre!");
                     break;
-                } else if (n == 1){
+                } else if (n == 1) {
                     act();
                 }
-=======
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
-                break;
-            case 3 :
+            break;
+
+            case 3:
                 System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextInt();
+                n1 = teclado.nextFloat();
                 System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextInt();
+                n2 = teclado.nextFloat();
                 resultado = n1 * n2;
                 System.out.println("O resultado da multiplicacao eh = " + resultado);
-<<<<<<< HEAD
                 System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
                 n = teclado.nextInt();
-                if (n == 0){
+                if (n == 0) {
                     System.out.println("Obrigado por usar a calculadora, volte sempre!");
                     break;
-                } else if (n == 1){
+                } else if (n == 1) {
                     act();
                 }
-=======
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
-                break;
-            case 4 :
+            break;
+
+            case 4:
                 System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextInt();
+                n1 = teclado.nextFloat();
                 System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextInt();
+                n2 = teclado.nextFloat();
                 resultado = n1 / n2;
                 System.out.println("O resultado da divisao eh = " + resultado);
-<<<<<<< HEAD
                 System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
                 n = teclado.nextInt();
-                if (n == 0){
+                if (n == 0) {
                     System.out.println("Obrigado por usar a calculadora, volte sempre!");
                     break;
-                } else if (n == 1){
+                } else if (n == 1) {
+                    act();
+                }
+            break;
+
+            case 5 :
+                System.out.println("Digite o primeiro numero: ");
+                d1 = teclado.nextDouble();
+                System.out.println("Digite o segundo numero: ");
+                d2 = teclado.nextDouble();
+                resultado = Math.pow(d1,d2);
+                System.out.println("O resultado da potenciaçao eh = " + resultado);
+                System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                n = teclado.nextInt();
+                if (n == 0) {
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
+                } else if (n == 1) {
                     act();
                 }
                 break;
-            default :
+
+            case 6 :
+                System.out.println("Digite o numero: ");
+                d1 = teclado.nextDouble();
+                resultado = Math.sqrt(d1);
+                System.out.println("O resultado da raiz eh = " + resultado);
+                System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                n = teclado.nextInt();
+                if (n == 0) {
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
+                } else if (n == 1) {
+                    act();
+                }
+                break;
+
+            case 7 :
+                resultado = 1;
+                System.out.println("Digite o numero: ");
+                d1 = teclado.nextDouble();
+                if (d1 <= 0){
+                    System.out.println("Resultado invalido, tente novamente");
+                    act();
+
+                }
+                for (int i = 1; i <= d1; i++){
+                    resultado *= i;
+                }
+                System.out.println("O resultado do fatorial de " + d1 + " eh = " + resultado);
+                System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                n = teclado.nextInt();
+                if (n == 0) {
+                    System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                    break;
+                } else if (n == 1) {
+                    act();
+                }
+                break;
+
+            case 8 :
+                System.out.println("Qual operaçao voce deseja realizar:");
+                System.out.println("1 - Seno de um angulo\n" +
+                        "2 - Cosseno de um angulo\n" +
+                        "3 - Tangente de um angulo\n");
+                n = teclado.nextInt();
+                if (n == 1){
+                    System.out.println("Digite o valor do angulo: ");
+                    d1 = teclado.nextDouble();
+                    double d3 = Math.toRadians(d1);
+                    System.out.println("O seno de " + d1 + " eh " + Math.sin(d3));
+                    System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                    n = teclado.nextInt();
+                    if (n == 0) {
+                        System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                        break;
+                    } else if (n == 1) {
+                        act();
+                    }
+                    break;
+                } else if (n == 2){
+                    System.out.println("Digite o valor do angulo: ");
+                    d1 = teclado.nextDouble();
+                    double d3 = Math.toRadians(d1);
+                    System.out.println("O cosseno de " + d1 + " eh " + Math.cos(d3));
+                    System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                    n = teclado.nextInt();
+                    if (n == 0) {
+                        System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                        break;
+                    } else if (n == 1) {
+                        act();
+                    }
+                    break;
+                } else if (n == 3){
+                    System.out.println("Digite o valor do angulo: ");
+                    d1 = teclado.nextDouble();
+                    double d3 = Math.toRadians(d1);
+                    System.out.println("A tangente de " + d1 + " eh " + Math.tan(d3));
+                    System.out.println("Deseja continuar? Se sim digite 1 se nao digite 0");
+                    n = teclado.nextInt();
+                    if (n == 0) {
+                        System.out.println("Obrigado por usar a calculadora, volte sempre!");
+                        break;
+                    } else if (n == 1) {
+                        act();
+                    }
+                    break;
+                }
+
+            default:
                 System.out.println("Opçao nao disponivel!!");
                 break;
-=======
-                break;
-            default :
-                System.out.println("Opçao nao disponivel");
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
         }
     }
 
+
     public static void main(String[] args) {
-        Calculadora z = new Calculadora();
-        z.act();
+        Calculadora calc = new Calculadora();
+        calc.act();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 1693d09f5ccd192ad5948e7e4261be95e047b42e
