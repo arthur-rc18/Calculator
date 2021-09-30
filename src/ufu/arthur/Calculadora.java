59a6e223bd3c7c6b8b44f75a0f2ce9dba0eaf9ca
Arthur <arthurrodri015@gmail.com>
2021-09-30 19:15:14 -0300

    Adding binary options

--- src/ufu/arthur/Calculadora.java
+++ src/ufu/arthur/Calculadora.java
@@ -11,186 +11,199 @@ public class Calculadora  {
         int n;
         float n1, n2;
         double d1, d2;
-        System.out.println("Ola, escolha qual operaçao voce deseja realizar: ");
-        System.out.println("1 - Soma\n" +
-                "2 - Subtraçao\n" +
-                "3 - Multiplicaçao\n" +
-                "4 - Divisao\n" +
-                "5 - Potencia\n" +
-                "6 - Raiz quadrada\n" +
-                "7 - Fatorial\n" +
-                "8 - Trigonometria\n" +
-                "9 - Porcentagem\n" +
-                "10 - Conversao\n" +
-                "0 - Para sair da calculadora\n");
-        System.out.println("Digite a opçao desejada:");
+        System.out.println("Qual calculadora voce deseja escolher?");
+        System.out.println("1 - Cientifica\n" +
+                "2 - Porgramador\n" +
+                "3 - Conversor\n");
+        n = teclado.nextInt();
 
-       n = teclado.nextInt();
+        switch (n){
+            case 1 :
+                System.out.println("Voce escolheu a calculadora cientifica!!!");
+                System.out.println("Escolha qual operaçao voce deseja realizar: ");
+                System.out.println("1 - Soma\n" +
+                        "2 - Subtraçao\n" +
+                        "3 - Multiplicaçao\n" +
+                        "4 - Divisao\n" +
+                        "5 - Potencia\n" +
+                        "6 - Raiz quadrada\n" +
+                        "7 - Fatorial\n" +
+                        "8 - Trigonometria\n" +
+                        "9 - Porcentagem\n" +
+                        "0 - Para sair da calculadora\n");
+                System.out.println("Digite a opçao desejada:");
 
-        switch(n){
-            case 1:
-                System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextFloat();
-                System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextFloat();
-                resultado = n1 + n2;
-                System.out.println("O resultado da soma eh = " + resultado);
-                cont();
-            break;
-
-            case 2:
-                System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextFloat();
-                System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextFloat();
-                resultado = n1 - n2;
-                System.out.println("O resultado da subtraçao eh = " + resultado);
-                cont();
-            break;
-
-            case 3:
-                System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextFloat();
-                System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextFloat();
-                resultado = n1 * n2;
-                System.out.println("O resultado da multiplicacao eh = " + resultado);
-               cont();
-            break;
-
-            case 4:
-                System.out.println("Digite o primeiro numero: ");
-                n1 = teclado.nextFloat();
-                System.out.println("Digite o segundo numero: ");
-                n2 = teclado.nextFloat();
-                resultado = n1 / n2;
-                System.out.println("O resultado da divisao eh = " + resultado);
-                cont();
-            break;
-
-            case 5 :
-                System.out.println("Digite o primeiro numero: ");
-                d1 = teclado.nextDouble();
-                System.out.println("Digite o segundo numero: ");
-                d2 = teclado.nextDouble();
-                resultado = Math.pow(d1,d2);
-                System.out.println("O resultado da potenciaçao eh = " + resultado);
-                cont();
-                break;
-
-            case 6 :
-                System.out.println("Digite o numero: ");
-                d1 = teclado.nextDouble();
-                resultado = Math.sqrt(d1);
-                System.out.println("O resultado da raiz eh = " + resultado);
-                cont();
-                break;
-
-            case 7 :
-                resultado = 1;
-                System.out.println("Digite o numero: ");
-                d1 = teclado.nextDouble();
-                if (d1 <= 0){
-                    System.out.println("Resultado invalido, tente novamente");
-                    act();
-
-                }
-                for (int i = 1; i <= d1; i++){
-                    resultado *= i;
-                }
-                System.out.println("O resultado do fatorial de " + d1 + " eh = " + resultado);
-                cont();
-                break;
-
-            case 8 :
-                System.out.println("Qual operaçao voce deseja realizar:");
-                System.out.println("1 - Seno de um angulo\n" +
-                        "2 - Cosseno de um angulo\n" +
-                        "3 - Tangente de um angulo\n" +
-                        "4 - Cossecante de um numero\n" +
-                        "5 - Secante de um numero\n" +
-                        "6 - Cotangente de um numero\n");
                 n = teclado.nextInt();
-                if (n == 1){
-                    System.out.println("Digite o valor do angulo: ");
-                    d1 = teclado.nextDouble();
-                    double d3 = Math.toRadians(d1);
-                    System.out.println("O seno de " + d1 + "º eh " + Math.sin(d3));
-                    cont();
-                    break;
-                } else if (n == 2){
-                    System.out.println("Digite o valor do angulo: ");
-                    d1 = teclado.nextDouble();
-                    double d3 = Math.toRadians(d1);
-                    System.out.println("O cosseno de " + d1 + "º eh " + Math.cos(d3));
-                    cont();
-                    break;
-                } else if (n == 3){
-                    System.out.println("Digite o valor do angulo: ");
-                    d1 = teclado.nextDouble();
-                    double d3 = Math.toRadians(d1);
-                    System.out.println("A tangente de " + d1 + "º eh " + Math.tan(d3));
-                    cont();
-                    break;
-                } else if (n == 4){
-                    System.out.println("Digite o valor do angulo: ");
-                    d1 = teclado.nextDouble();
-                    double d3 = Math.toRadians(d1);
-                    if (d1 == 0 || d1 == 180 || d1 == 360){
-                        System.out.println("Angulo indefinido");
+
+                switch(n){
+                    case 1:
+                        System.out.println("Digite o primeiro numero: ");
+                        n1 = teclado.nextFloat();
+                        System.out.println("Digite o segundo numero: ");
+                        n2 = teclado.nextFloat();
+                        resultado = n1 + n2;
+                        System.out.println("O resultado da soma eh = " + resultado);
                         cont();
                         break;
-                    } else if (d1 != 0) {
-                        double d4 = 1 / Math.sin(d3);
-                        System.out.println("A cossecante de " + d3 + "º eh " + d4);
+
+                    case 2:
+                        System.out.println("Digite o primeiro numero: ");
+                        n1 = teclado.nextFloat();
+                        System.out.println("Digite o segundo numero: ");
+                        n2 = teclado.nextFloat();
+                        resultado = n1 - n2;
+                        System.out.println("O resultado da subtraçao eh = " + resultado);
+                        cont();
+                        break;
+
+                    case 3:
+                        System.out.println("Digite o primeiro numero: ");
+                        n1 = teclado.nextFloat();
+                        System.out.println("Digite o segundo numero: ");
+                        n2 = teclado.nextFloat();
+                        resultado = n1 * n2;
+                        System.out.println("O resultado da multiplicacao eh = " + resultado);
+                        cont();
+                        break;
+
+                    case 4:
+                        System.out.println("Digite o primeiro numero: ");
+                        n1 = teclado.nextFloat();
+                        System.out.println("Digite o segundo numero: ");
+                        n2 = teclado.nextFloat();
+                        resultado = n1 / n2;
+                        System.out.println("O resultado da divisao eh = " + resultado);
+                        cont();
+                        break;
+
+                    case 5 :
+                        System.out.println("Digite o primeiro numero: ");
+                        d1 = teclado.nextDouble();
+                        System.out.println("Digite o segundo numero: ");
+                        d2 = teclado.nextDouble();
+                        resultado = Math.pow(d1,d2);
+                        System.out.println("O resultado da potenciaçao eh = " + resultado);
+                        cont();
+                        break;
+
+                    case 6 :
+                        System.out.println("Digite o numero: ");
+                        d1 = teclado.nextDouble();
+                        resultado = Math.sqrt(d1);
+                        System.out.println("O resultado da raiz eh = " + resultado);
+                        cont();
+                        break;
+
+                    case 7 :
+                        resultado = 1;
+                        System.out.println("Digite o numero: ");
+                        d1 = teclado.nextDouble();
+                        if (d1 <= 0){
+                            System.out.println("Resultado invalido, tente novamente");
+                            act();
+
+                        }
+                        for (int i = 1; i <= d1; i++){
+                            resultado *= i;
+                        }
+                        System.out.println("O resultado do fatorial de " + d1 + " eh = " + resultado);
+                        cont();
+                        break;
+
+                    case 8 :
+                        System.out.println("Qual operaçao voce deseja realizar:");
+                        System.out.println("1 - Seno de um angulo\n" +
+                                "2 - Cosseno de um angulo\n" +
+                                "3 - Tangente de um angulo\n" +
+                                "4 - Cossecante de um numero\n" +
+                                "5 - Secante de um numero\n" +
+                                "6 - Cotangente de um numero\n");
+                        n = teclado.nextInt();
+                        if (n == 1){
+                            System.out.println("Digite o valor do angulo: ");
+                            d1 = teclado.nextDouble();
+                            double d3 = Math.toRadians(d1);
+                            System.out.println("O seno de " + d1 + "º eh " + Math.sin(d3));
+                            cont();
+                            break;
+                        } else if (n == 2){
+                            System.out.println("Digite o valor do angulo: ");
+                            d1 = teclado.nextDouble();
+                            double d3 = Math.toRadians(d1);
+                            System.out.println("O cosseno de " + d1 + "º eh " + Math.cos(d3));
+                            cont();
+                            break;
+                        } else if (n == 3){
+                            System.out.println("Digite o valor do angulo: ");
+                            d1 = teclado.nextDouble();
+                            double d3 = Math.toRadians(d1);
+                            System.out.println("A tangente de " + d1 + "º eh " + Math.tan(d3));
+                            cont();
+                            break;
+                        } else if (n == 4){
+                            System.out.println("Digite o valor do angulo: ");
+                            d1 = teclado.nextDouble();
+                            double d3 = Math.toRadians(d1);
+                            if (d1 == 0 || d1 == 180 || d1 == 360){
+                                System.out.println("Angulo indefinido");
+                                cont();
+                                break;
+                            } else if (d1 != 0) {
+                                double d4 = 1 / Math.sin(d3);
+                                System.out.println("A cossecante de " + d3 + "º eh " + d4);
+                                cont();
+
+                            }
+                        } else if (n == 5){
+                            System.out.println("Digite o valor do angulo: ");
+                            d1 = teclado.nextDouble();
+                            double d3 = Math.toRadians(d1);
+                            double d4 = 1 / Math.cos(d3);
+                            if (Math.cos(d3) == 0){
+                                System.out.println("Angulo indefinido");
+                                cont();
+                                break;
+                            } else if (d1 == 90 || d1 == 270){
+                                System.out.println("Angulo indefinido");
+                                cont();
+                                break;
+                            } else
+                                System.out.println("A secante de " + d3 + "º eh " + d4);
+                            cont();
+                            break;
+                        } else if (n == 6){
+                            System.out.println("Digite o valor do angulo: ");
+                            d1 = teclado.nextDouble();
+                            double d3 = Math.toRadians(d1);
+                            double d4 = 1 / Math.tan(d3);
+                            if (d1 == 0){
+                                System.out.println("Angulo indefinido");
+                                cont();
+                                break;
+                            }
+                            System.out.println("A cotangente de " + d3 + "º eh " + d4);
+                            cont();
+                        }
+
+                    case 9 :
+                        System.out.println("Digite o valor da porcentagem:");
+                        d1 = teclado.nextDouble();
+                        System.out.println("Digite o valor do numero:");
+                        d2 = teclado.nextDouble();
+                        resultado = d2 * (d1/100);
+                        System.out.println("O valor de " + d1 + "% " + " de " + d2 + " eh " + resultado);
                         cont();
 
-                    }
-                } else if (n == 5){
-                    System.out.println("Digite o valor do angulo: ");
-                    d1 = teclado.nextDouble();
-                    double d3 = Math.toRadians(d1);
-                    double d4 = 1 / Math.cos(d3);
-                    if (Math.cos(d3) == 0){
-                        System.out.println("Angulo indefinido");
-                        cont();
+                    default :
+                        System.out.println("Opçao indisponivel");
                         break;
-                    } else if (d1 == 90 || d1 == 270){
-                        System.out.println("Angulo indefinido");
-                        cont();
-                        break;
-                    } else
-                        System.out.println("A secante de " + d3 + "º eh " + d4);
-                        cont();
-                        break;
-                } else if (n == 6){
-                    System.out.println("Digite o valor do angulo: ");
-                    d1 = teclado.nextDouble();
-                    double d3 = Math.toRadians(d1);
-                    double d4 = 1 / Math.tan(d3);
-                    if (d1 == 0){
-                        System.out.println("Angulo indefinido");
-                        cont();
-                        break;
-                    }
-                    System.out.println("A cotangente de " + d3 + "º eh " + d4);
-                    cont();
-                }
+        }
 
-            case 9 :
-                System.out.println("Digite o valor da porcentagem:");
-                d1 = teclado.nextDouble();
-                System.out.println("Digite o valor do numero:");
-                d2 = teclado.nextDouble();
-                resultado = d2 * (d1/100);
-                System.out.println("O valor de " + d1 + "% " + " de " + d2 + " eh " + resultado);
-                cont();
 
-            case 10 :
+            case 2 :
+                System.out.println("Voce escolheu a calculadora de programador!!!");
                 System.out.println("Qual conversao voce deseja realizar:");
-                System.out.println("1 - Bases numericas\n" +
-                        "2 - Cambio\n" +
-                        "3 - Medidas\n");
+                System.out.println("1 - Bases numericas\n");
                 n = teclado.nextInt();
                 if (n == 1){
                     System.out.println("Qual base voce deseja converter?");
@@ -216,7 +229,11 @@ public class Calculadora  {
                     } else if (n == 2){
                         System.out.println("Digite o numero em decimal");
                         int dec = teclado.nextInt();
+                        System.out.println("A conversao de " + dec + " para a base binaria eh: ");
                         resto(dec);
+                        System.out.println("\n--------------\n");
+                        cont();
+                        break;
                     }
                 }
 
@@ -244,13 +261,9 @@ public class Calculadora  {
     }
 
     public void resto(int n1){
-        int f = n1;
-        int x = 0;
-        while(f > 0){
-            f = n1 % 2;
-            x = f;
-            System.out.println(f);
-            resto(f);
+        if (n1 > 0){
+            resto(n1 / 2);
+            System.out.print(n1 % 2);
         }
     }
 }
